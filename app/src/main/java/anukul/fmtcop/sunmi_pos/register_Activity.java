84a78beffda4ae37571bb37d71312ddb305f4d92
taeholiday <anukul.fmt@gmail.com>
2020-10-26 09:26:39 +0700

    commit

--- app/src/main/java/anukul/fmtcop/sunmi_pos/register_Activity.java
+++ app/src/main/java/anukul/fmtcop/sunmi_pos/register_Activity.java
@@ -3,8 +3,11 @@ package anukul.fmtcop.sunmi_pos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class register_Activity extends AppCompatActivity {
+
+    private Button io;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
